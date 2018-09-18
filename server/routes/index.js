b83a2b3293901d22015ad10450f4287cdcb1e3fe
John <joedw1978@gmail.com>
2018-09-18 14:17:05 -0400

    test

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ var Post = mongoose.model("Post", postSchema);
 /* GET home page. */
 router.get("/", function(req, res, next) {
   Post.find({}, (err, posts) => {
-    res.render("dist/index.html", { posts: posts });
+    res.json({ posts: posts });
   });
 });
 

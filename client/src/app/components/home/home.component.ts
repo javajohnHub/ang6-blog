7426018035839d09d351ed3d307bc75c84b870d0
John <joedw1978@gmail.com>
2018-09-18 14:55:35 -0400

    changes

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: 'home.component.html'
 })
 export class HomeComponent {
-	constructor() {}
+	body;
+	constructor(private _http: HttpClient) {}
 
 	ngOnInit() {}
+
+	post() {
+		this._http
+			.post<any>('https://ang6-blog.herokuapp.com/addpost', {
+				body: this.body
+			})
+			.subscribe(postReply => {
+				console.log(postReply);
+			});
+	}
 }

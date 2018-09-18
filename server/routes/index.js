002f95a441a75663475a25070138c29bbd208059
John <joedw1978@gmail.com>
2018-09-18 13:24:52 -0400

    post

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var postSchema = new mongoose.Schema({ body: String });
+var Post = mongoose.model("Post", postSchema);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendfile('dist/index.html');
+router.get("/", function(req, res, next) {
+  Post.find({}, (err, posts) => {
+    res.sendfile("dist/index.html");
+  });
 });
 
+app.post("/addpost", (req, res) => {
+  var postData = new Post(req.body);
+  postData
+    .save()
+    .then(result => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      res.status(400).send("Unable to save data");
+    });
+});
 module.exports = router;
